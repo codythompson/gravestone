6da595d9e2495bdc9a29768811128318e5b9843b
codythompson <codythompson22@gmail.com>
2023-10-21 01:24:16 -0700

    added a routine, which wraps multiple tween objects. Also made a routine-machine (factory) for easy chaining

--- Neotweens.py
+++ Neotweens.py
@@ -1,4 +1,4 @@
-# from enum import Flag, auto
+import time
 from collections import namedtuple
 from neopixel import NeoPixel
 
@@ -14,19 +14,20 @@ def clampRGB(value:int)->int:
 def clampW(value:float)->float:
   return clamp(0, 1, value)
 
-def createClampedColor(r:int, g:int, b:int, w:float)->ColorTuple:
-  if (w > 2 or w < -1):
-    print("warning: received white value more than 2x the min or max value. expected 0.0-1.0, got", w)
+def clampColor(color:ColorTuple)->ColorTuple:
+  (r,g,b,w) = color
+  if w > 10 or w < -9:
+    print("warning: received white value more than 10x the min or max value. expected 0.0-1.0, got", w)
   return ColorTuple(clampRGB(r), clampRGB(g), clampRGB(b), clampW(w))
 
 def addColors(a:ColorTuple, b:ColorTuple)->ColorTuple:
-  return createClampedColor(a.r + b.r, a.g + b.g, a.b + b.b, a.w + b.w)
+  return ColorTuple(a.r + b.r, a.g + b.g, a.b + b.b, a.w + b.w)
 
 def subtractColors(a:ColorTuple, b:ColorTuple)->ColorTuple:
-  return createClampedColor(a.r - b.r, a.g - b.g, a.b - b.b, a.w - b.w)
+  return ColorTuple(a.r - b.r, a.g - b.g, a.b - b.b, a.w - b.w)
 
 def multiplyColor(color:ColorTuple, multiplyBy:float)->ColorTuple:
-  return createClampedColor(color.r * multiplyBy, color.g * multiplyBy, color.b * multiplyBy, color.w * multiplyBy)
+  return ColorTuple(color.r * multiplyBy, color.g * multiplyBy, color.b * multiplyBy, color.w * multiplyBy)
 
 class NeoPixelGroup:
   _strands: [NeoPixel]
@@ -49,14 +50,10 @@ class NeoPixelGroup:
   def __len__(self):
     return len(self._strands)
 
-  def add(self, strand:NeoPixel, index:int, offsetFromPrevious:float=0) -> int:
-    previousOffset = 0
-    if (len(self) > 0):
-      # we store the cumulative offset so we don't have to re-calculate ever time we set the color
-      previousOffset = self.get(len(self._offsets)-1)[2]
+  def add(self, strand:NeoPixel, index:int, offset:float=0) -> int:
     self._strands.append(strand)
     self._indices.append(index)
-    self._offsets.append(previousOffset+offsetFromPrevious)
+    self._offsets.append(offset)
 
     return len(self._strands)-1
 
@@ -67,20 +64,21 @@ class NeoPixelGroup:
     (strand,strandIndex,unused) = self.get(index)
     strand[strandIndex] = color
 
+  def getMaxOffset(self)->float:
+   return max(offset for offset in self._offsets)
+
   def getLocalProgress(self, index:int, tweenProgress:float)->float:
     offset = self.get(index)[2]
     return tweenProgress - offset
 
   def fill(self, color:ColorTuple) -> None:
-    # print(self.name)
     for i in range(len(self)):
       strand = self._strands[i]
       index = self._indices[i]
-      # print(index)
       strand[index] = color
   
   def showAll(self)->None:
-      [strand.show() for [strand] in self]
+    [strand.show() for strand in self._strands]
 
 # class ColorChannel(Flag):
 #   RED=auto()
@@ -95,7 +93,7 @@ class NeoTween:
   fromColor:ColorTuple
   toColor:ColorTuple
   duration:int
-  delay:int = 0
+  delay:int
 
   __slots__ = "groups", "name", "fromColor", "toColor", "duration", "delay", "add", "getColor", "setProgress"
 
@@ -106,14 +104,21 @@ class NeoTween:
       fromColor:ColorTuple,
       toColor:ColorTuple,
       duration:int,
-      delay:int = 0
+      delay:int = 0,
+      groups:[NeoPixel] = []
     ):
       self.name = name
-      self.groups = []
+      self.groups = groups
       self.fromColor = fromColor
       self.toColor = toColor
       self.duration = duration
       self.delay = delay
+
+  def getMaxOffset(self)->float:
+    return max(group.getMaxOffset() for group in self.groups)
+
+  def getDurationWithDelayAndOffsets(self)->float:
+    return self.delay + self.duration + (self.duration * self.getMaxOffset())
 
   def add(self, group:NeoPixelGroup)->int:
     self.groups.append(group)
@@ -122,10 +127,114 @@ class NeoTween:
   def getColor(self, progress:float)->ColorTuple:
     # TODO - variable easing funcs
     # linear scaling: from + progress*(from-to)
-    return addColors(self.fromColor, multiplyColor(subtractColors(self.toColor, self.fromColor), progress))
+    return clampColor(addColors(self.fromColor, multiplyColor(subtractColors(self.toColor, self.fromColor), progress)))
 
   def setProgress(self, progress:float)->None:
     for group in self.groups:
       for i in range(len(group)):
-        localProgress = group.getLocalProgress(i, progress) % 1 # TODO - put the mod1 rolover behind a setting flag
-        group.set(i, self.getColor(localProgress))
+        localProgress = group.getLocalProgress(i, progress)
+        if localProgress >= 0 and localProgress <= 1:
+          group.set(i, self.getColor(localProgress))
+        # future todo: if self.doWraparound and localProgress < 0: localProgress = 1 + localProgress
+
+class NeoTweenRoutine:
+  name:str
+  tweens:[NeoTween]
+  _startedAt:float
+
+  def __init__(self, name:str):
+    self.name = name
+    self.tweens = []
+    self._startedAt = None
+
+  def start(self):
+    self._startedAt = time.monotonic()
+
+  def getDuration(self)->float:
+    return max(thingy.getDurationWithDelayAndOffsets() for thingy in self.tweens)
+
+  def updateTween(self, tween:NeoTween, delta:float)->None:
+    progress = (delta - tween.delay) / tween.duration
+    tween.setProgress(progress)
+
+  def update(self)->None:
+    now = time.monotonic()
+    if self._startedAt == None:
+      return
+
+    duration = self.getDuration()
+    delta = (now - self._startedAt) % duration
+    # TODO - does this work? / test it
+    [self.updateTween(tween, delta) for tween in self.tweens]
+
+class NeoTweenRoutineMachine:
+  groups:[NeoPixelGroup]
+  routine:NeoTweenRoutine
+  relativeDelays:[float]
+
+  def __init__(self,*, name:str, groups:NeoPixelGroup, initialColor:ColorTuple, duration:float=1.0, delay:float=0.0):
+    self.groups = groups
+    self.routine = NeoTweenRoutine(name)
+    self.relativeDelays=[]
+    self.add(fromColor=initialColor,toColor=initialColor,duration=duration,delay=delay)
+
+  def getNextTweenName(self)->str:
+    return str.format("{}-{}", self.routine.name, len(self.routine.tweens))
+
+  def add(self, *, fromColor:ColorTuple, toColor:ColorTuple, duration:float, delay:float=0.0, name:str=None):
+    if name == None:
+      name = self.getNextTweenName()
+    self.routine.tweens.append(NeoTween(
+      name=name,
+      toColor=toColor,
+      fromColor=fromColor,
+      duration=duration,
+      groups=self.groups
+    ))
+    self.relativeDelays.append(delay)
+    self.getLastTween().delay = self.calculateStartElapsedTime()
+
+  def getLastTween(self)->NeoTween:
+    return self.routine.tweens[len(self.routine.tweens)-1]
+
+  def calculateStartElapsedTime(self, index:int=None)->float:
+    if index == None:
+      index = len(self.relativeDelays)-1
+    if index < 0:
+      return 0.0
+    delay = self.relativeDelays[index]
+    if index == 0:
+      return delay
+    else:
+      prevDuration = self.routine.tweens[index-1].duration
+      result = delay + prevDuration + self.calculateStartElapsedTime(index-1)
+      return result
+  
+  def toColor(self, r:int,g:int,b:int,w:float):
+    self.getLastTween().toColor = ColorTuple(r,g,b,w)
+    return self
+
+  def forDuration(self, duration:float):
+    self.getLastTween().duration = duration
+    return self
+
+  def delayedBy(self, delay:float):
+    self.relativeDelays[len(self.relativeDelays)-1] = delay
+    self.getLastTween().delay = self.calculateStartElapsedTime()
+    return self
+
+  def then(self, *, fromColor=None, toColor=None, duration:float=None, delay:float=0.0, name:str=None):
+    current = self.getLastTween()
+
+    if fromColor == None:
+      fromColor = current.fromColor
+    if toColor == None:
+      toColor = fromColor
+    if duration == None:
+      duration = current.duration
+    self.add(fromColor=fromColor, toColor=toColor, duration=duration, delay=delay, name=name)
+    return self
+
+  def done(self)->NeoTweenRoutine:
+    return self.routine
+
--- main.py
+++ main.py
@@ -2,52 +2,107 @@
 import time
 import board
 import neopixel
-from Neotweens import ColorTuple, NeoPixelGroup, NeoTween
+from Neotweens import ColorTuple, NeoPixelGroup, NeoTween, NeoTweenRoutine, NeoTweenRoutineMachine
 
 board_pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
-board_pixel.brightness = 0.1
+board_pixel.brightness = 0.08
 
-strand1 = neopixel.NeoPixel(board.A0, 4, pixel_order="GRBW")
-strand1.brightness = 0.1
+strand1 = neopixel.NeoPixel(board.A0, 8, pixel_order="GRBW")
+strand1.brightness = 0.08
 
-groupA = NeoPixelGroup("grpA")
-groupA.add(board_pixel, 0)
+# groupA = NeoPixelGroup("grpA")
+# groupA.add(board_pixel, 0)
+# groupA.add(strand1, 0)
 groupB = NeoPixelGroup("grpB")
-groupB.add(strand1, 0, 0.00)
-groupB.add(strand1, 1, 0.05)
-groupB.add(strand1, 2, 0.05)
-groupB.add(strand1, 3, 0.05)
+# groupB.add(strand1, 0, 0.00)
+offset=0.2
+for i in range(8):
+  groupB.add(strand1, i, (i*offset)%1)
 
-tweenA = NeoTween(
-  name="tweenA",
-  fromColor=ColorTuple(100, 100, 255, 0),
-  toColor=ColorTuple(255, 0, 0, 0),
-  duration=5
+# tweenA = NeoTween(
+#   name="tweenA",
+#   fromColor=ColorTuple(100, 100, 255, 0),
+#   toColor=ColorTuple(255, 0, 0, 0),
+#   duration=1.5
+# )
+# tweenA.add(groupA)
+# tweenB = NeoTween(
+#   name="tweenB",
+#   fromColor=ColorTuple(100, 100, 255, 0),
+#   toColor=ColorTuple(0, 255, 0, 0.0),
+#   duration=0.5,
+#   delay=4
+# )
+# tweenB.add(groupB)
+# tweenB2 = NeoTween(
+#   name="tweenB2",
+#   fromColor=ColorTuple(0, 255, 0, 0.0),
+#   toColor=ColorTuple(0, 255, 0, 0.0),
+#   duration=2,
+#   delay=0.5
+# )
+# tweenB2.add(groupB)
+# tweenC = NeoTween(
+#   name="tweenC",
+#   fromColor=ColorTuple(0, 255, 0, 0.0),
+#   toColor=ColorTuple(100, 100, 255, 0),
+#   duration=0.5,
+#   delay=8.5
+# )
+# tweenC.add(groupB)
+# tweenC2 = NeoTween(
+#   name="tweenC2",
+#   fromColor=ColorTuple(100, 100, 255, 0),
+#   toColor=ColorTuple(100, 100, 255, 0),
+#   duration=1,
+#   delay=5
+# )
+# tweenC2.add(groupB)
+
+# routine1 = NeoTweenRoutine("routine1")
+# routine1.tweens.append(tweenA)
+# routine1.tweens.append(tweenB)
+# routine1.tweens.append(tweenB2)
+# routine1.tweens.append(tweenC)
+# routine1.tweens.append(tweenC2)
+# routine1.start()
+
+m1 = NeoTweenRoutineMachine(
+   name="routine1",
+   groups=[groupB],
+   initialColor=ColorTuple(0, 0, 255, 0)
 )
-tweenA.add(groupA)
-tweenB = NeoTween(
-  name="tweenB",
-  fromColor=ColorTuple(255, 0, 0, 0),
-  toColor=ColorTuple(0, 255, 0, 2.1),
-  duration=1
-)
-tweenB.add(groupB)
+m1.delayedBy(1)
+m1.forDuration(0.25)
+m1.toColor(100,0,0,0.5)
+
+m1.then(name="toYellow")
+m1.delayedBy(1)
+m1.toColor(50,50,0,0.0)
+
+m1.then(name="toGreen")
+m1.delayedBy(1)
+m1.toColor(0,100,0,0.0)
+
+m1.then(name="toCyan")
+m1.delayedBy(1)
+m1.toColor(0,100,100,0.5)
+
+m1.then(name="toBlue")
+m1.toColor(0,0,100,0.5)
+m1.toColor(0,0,255,0)
+m1.delayedBy(1)
+routine1 = m1.done()
 
 last_change = time.monotonic()
-start_time = last_change
-current_prog = 1
-duration = 3
+routine1.start()
 
 while True:
     now = time.monotonic()
-    current_prog = (now - start_time) / duration
-    if current_prog >= 1:
-        current_prog = current_prog % 1
-        start_time = now
-    if now - last_change > 0.1:
+    if now - last_change > 0.02:
+    # if now - last_change > 0.1:
         last_change = now
-        tweenA.setProgress(current_prog)
-        tweenB.setProgress(current_prog)
+        routine1.update() # TODO - simply test this
 
 
 
